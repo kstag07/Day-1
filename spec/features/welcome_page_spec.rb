7206dc28b1370772a877201488f37f4720a71961
kstag07 <kstag07@yahoo.com>
2014-05-07 18:44:36 -0700

    Delete welcome_page_spec.rb

--- spec/features/welcome_page_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe "initial rails app welcome page" do
-  it "has a welcome headline" do
-    visit "http://localhost:3000"
-    page.text.must_include "Welcome aboard"
-  end
-
-end
