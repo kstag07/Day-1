f5295a3969467edf390bb9f5d2760aa5f6468116
kstag07 <kstag07@yahoo.com>
2014-05-07 18:44:22 -0700

    Delete rails_new_spec.rb

--- spec/features/rails_new_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "My Rails App welcome page" do
-    it "shows the welcome message" do
-    visit "http://localhost:3000"
-    page.text.mustinclude "Welcome Aboard"
-  end
-end
